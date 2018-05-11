d1dada08a53ce14bb18bdc1df6d89729502164e3
your_name <1594051054@qq.com>
2018-05-11 11:44:46 +0800

    change views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
 
 from django.shortcuts import render
 
 def home_page(request):
-     return render(request, 'blog/home_page.html')
+
+    if request.method == 'POST':
+        comment = request.POST.get('comment')
+        user = request.POST.get("username")
+
+        print(comment * 10)
+        return render(request, 'blog/home_page.html', {'comment': comment, 'username': user})
+
+    return render(request, 'blog/home_page.html')
